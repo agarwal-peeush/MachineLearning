6394f1609eaad172d2858a6999ae8e7a1cce7e96
Peeush Agarwal <aryanpiyushrahul@gmail.com>
2018-10-29 12:03:41 +0530

    AndrewNg Coursera Machine Learning course in Python first commit

---
+++ Python/AndrewNgMLCourse/Ex1/Ex1.py
@@ -0,0 +1,18 @@
+# Machine Learning course by AndrewNg on Coursera
+
+# import all libraries required here
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 1. Print 5 by 5 Identity matrix
+
+id5 = np.identity(5)
+print(id5)
+
+# 2. Plot the data
+data = pd.read_csv('ex1data1.txt', header=None) # read from dataset
+X = data.iloc[:,0] # read first column
+y = data.iloc[:,1] # read second column
+m = len(y) # number of training example
+print(data.head()) # view first few rows of the data
