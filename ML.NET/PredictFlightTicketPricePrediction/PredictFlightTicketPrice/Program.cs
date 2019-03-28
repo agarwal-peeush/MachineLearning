ea55ff30a2bfb1223fd14f8fc38fda8eb152866e
Peeush Agarwal <agarwal.peeush@hotmail.com>
2019-03-28 23:36:17 +0530

    Prepare ConsoleApplication with ML.NET package installed

---
+++ ML.NET/PredictFlightTicketPricePrediction/PredictFlightTicketPrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PredictFlightTicketPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
